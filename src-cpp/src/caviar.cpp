8b7d2ad7e7d6185c2bc616386b18027e7df1ee3d
k3yavi <avi.srivastava@stonybrook.edu>
2017-07-30 02:45:09 -0400

    stupid version

--- src-cpp/src/caviar.cpp
+++ src-cpp/src/caviar.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
+#include <string>
 
 #include "gff.h"
 
 int main(int argc, char* argv[]) {
 
-    if (argc == 1 or argc > 2) {
-        std::cerr << "Usage: TestGFFParse input\n";
+    if (argc == 1 or argc > 4 or argc < 4) {
+        std::cerr << "Usage: caviar [GTF] [pos] [out]\n";
         std::exit(1);
     }
 
@@ -15,18 +17,37 @@ int main(int argc, char* argv[]) {
     reader.readAll(true);
 
     std::cerr << "had count of " << reader.gflst.Count() << "\n";
+
     size_t nfeat = reader.gflst.Count();
-    for (size_t i=0; i < nfeat; ++i) {
-       GffObj* f = reader.gflst[i];
 
-       if(f->exonOverlap(13453, 13454)){
-         int idx = static_cast<int>(f->exonOverlapIdx(13453, 13454));
-         GffExon* ex = f->exons[idx];
+    uint64_t start, end, count{0};
+    std::string line;
+    std::ifstream posfile (argv[2]);
+    std::ofstream wfile (argv[3]);
 
-         std::cout << f->getID() << '\t' << f->getGeneID() << "\t"
-                   << ex->getAttr(f->names, "exon_id") << std::endl;
-       }
+    if (posfile.is_open() and wfile.is_open()){
+      while ( getline (posfile, line) ){
+        count += 1;
+        start = std::stoi(line, nullptr, 10) << '\n';
+        end = start + 30;
+
+        for (size_t i=0; i < nfeat; ++i) {
+          GffObj* f = reader.gflst[i];
+          if(f->exonOverlap(start, end)){
+            int idx = static_cast<int>(f->exonOverlapIdx(start, end));
+            GffExon* ex = f->exons[idx];
+            wfile << f->getID() << '\t'
+                      << ex->getAttr(f->names, "exon_id") << std::endl;
+          }
+        }
+
+        if(count % 10000 == 0){
+          std::cout << "\r Done: " << count <<" lines";
+        }
+      }
     }
+    posfile.close();
+    wfile.close();
     std::cout << "Exit Success";
     return 0;
 }
